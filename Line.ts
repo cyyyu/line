c3a86830a7cd1520d57b485866300f51b8f55635
Chuang Yu <cyu9960@gmail.com>
2019-04-04 17:03:50 +0800

    Initial commit

---
+++ Line.ts
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+export interface Options {
+  canvas: HTMLCanvasElement;
+  data: number[];
+}
+
+export default class Line {
+  constructor(options: Options) {}
+}
---
+++ index.ts
@@ -0,0 +1,5 @@
+import Line, { Options } from "./Line";
+
+export default function(options: Options): Line {
+  return new Line(options);
+}
